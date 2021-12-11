7e7f9a3cf9fb4604c837411c5a90f2a6ac38c0b3
Chang Yin Cheng <changyc9928@gmail.com>
2021-12-11 11:45:09 +0800

    implemented command to query primogem transection

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 import datetime
 import asyncio
+import genshin
 
 from database.coop import Coop
 from discord.ext import commands
@@ -60,6 +61,63 @@ async def coop(ctx: commands.Context):
     msg = await ctx.send("@everyone Hi Travalers, are you coming today?\n -- Pressing skipping button will clear all your data.\n -- Click on change time button to delay or move forward your online time (default: 10.30 pm).", view=AttendingView(bot), embed=embed)
     Coop.message_id = msg.id
 
+@bot.command()
+async def primo(ctx: commands.Context, url: str=None):
+    patch = [
+        datetime.datetime(2020, 9, 28, tzinfo=datetime.timezone(datetime.timedelta(hours=8))),
+        datetime.datetime(2020, 11, 11, tzinfo=datetime.timezone(datetime.timedelta(hours=8))),
+        datetime.datetime(2020, 12, 23, tzinfo=datetime.timezone(datetime.timedelta(hours=8))),
+        datetime.datetime(2021, 2, 3, tzinfo=datetime.timezone(datetime.timedelta(hours=8))),
+        datetime.datetime(2021, 3, 17, tzinfo=datetime.timezone(datetime.timedelta(hours=8))),
+        datetime.datetime(2021, 4, 28, tzinfo=datetime.timezone(datetime.timedelta(hours=8))),
+        datetime.datetime(2021, 6, 9, tzinfo=datetime.timezone(datetime.timedelta(hours=8))),
+        datetime.datetime(2021, 7, 21, tzinfo=datetime.timezone(datetime.timedelta(hours=8))),
+        datetime.datetime(2021, 9, 1, tzinfo=datetime.timezone(datetime.timedelta(hours=8))),
+        datetime.datetime(2021, 10, 13, tzinfo=datetime.timezone(datetime.timedelta(hours=8))),
+        datetime.datetime(2021, 11, 24, tzinfo=datetime.timezone(datetime.timedelta(hours=8))),
+        datetime.datetime(2022, 1, 5, tzinfo=datetime.timezone(datetime.timedelta(hours=8))),
+    ]
+    patch_no = [
+        "1.0",
+        "1.1",
+        "1.2",
+        "1.3",
+        "1.4",
+        "1.5",
+        "1.6",
+        "2.0",
+        "2.1",
+        "2.2",
+        "2.3",
+        "2.4"
+    ]
+    cookies = {"ltuid": 131897908, "ltoken": "PUvLWxC9lWijYCyi8ewhsxj3riKLc763kB85JPuH"}
+    client = genshin.GenshinClient(cookies)
+    client.authkey = genshin.get_authkey()
+    freq = {}
+    async for trans in client.transaction_log("primogem"):
+        key = ""
+        for i in range(1, len(patch)):
+            if patch[i-1] <= trans.time < patch[i]:
+                key = patch_no[i-1]
+        if key not in freq:
+            freq[key] = {}
+        if trans.reason not in freq[key]:
+            freq[key][trans.reason] = 0
+        freq[key][trans.reason] += trans.amount
+    # ret = ""
+    for key, value in freq.items():
+        ret = ""
+        total = 0
+        ret += f"{key}: \n"
+        for reason, amount in value.items():
+            ret += f"{reason}: {amount}\n"
+            total += amount if amount > 0 else 0
+        ret += f"**Total: {total}**\n"
+        ret += "\n\n"
+        await ctx.send(ret)
+
+    await client.close()
 
 load_dotenv()
 bot.run(os.getenv('TOKEN'))
