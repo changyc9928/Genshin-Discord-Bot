e7a8d2cb6eadf8d364e8de1ca84a3ebeb6ee0eec
Chang Yin Cheng <changyc9928@gmail.com>
2022-02-27 19:13:50 +0800

    added try catch at daily image query function

--- query_graphql.py
+++ query_graphql.py
@@ -25,7 +25,12 @@ async def query_image():
     )
 
     # Execute the query on the transport
-    result = await client.execute_async(query)
+    flag = True
+    while flag:
+        try:
+            result = await client.execute_async(query)
+        except:
+            pass
     img = result["getDaily"]["image"]
 
     file = io.BytesIO(b64decode(img[9:]))
@@ -64,7 +69,8 @@ async def query_artifact_domains():
             pass
     ret = {}
     for domain in result["getAllDomainCategory"]:
-        ret[domain["name"]] = "/".join(x.replace("_", " ").title() for x in domain["artifacts"])
+        ret[domain["name"]] = "/".join(x.replace("_", " ").title()
+                                       for x in domain["artifacts"])
     return ret
 
 
